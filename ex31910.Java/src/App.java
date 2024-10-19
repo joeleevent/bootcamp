591342c4215eb3a2a9897157eb5633264e535047
Joe Lee <joelee@Joes-MacBook-Air.local>
2024-10-19 17:03:37 +0800

    update

--- ex31910.Java/src/App.java
+++ ex31910.Java/src/App.java
@@ -1,5 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+int targetNumber = random.nextInt()+1;
+int guess = 0;
+
+while(){
+guess =scanner.nextInt();
+
+
+
+
+
+    if(){
+Sysyem.out.println("Too high.");
+    }else if {
+        System.out.println ("Too low.");
+    }else { 
+        System.out.println("Correct! The number is +" + targetNumber);
+    }
+    }
+    scanner.close();
+}
+
+
+
+}
+
     }
 }
